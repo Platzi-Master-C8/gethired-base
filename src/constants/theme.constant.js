450c3970c902c8321db8385fdbcd86a88a68dc7f
Jose Vargas <ruedagato@gmail.com>
2021-11-21 22:07:04 -0500

    init commit

---
+++ src/constants/theme.constant.js
@@ -0,0 +1,76 @@
+import {createTheme} from "@mui/material";
+import {COLORS} from "./colors.constants";
+
+export const THEME = createTheme({
+    typography: {
+        fontFamily: ['Montserrat', 'Mulish'].join(','),
+        h1: {
+            fontFamily: 'Mulish',
+            fontSize: 40,
+            fontWeight: 700,
+            lineHeight: '50.2px'
+        },
+        h2: {
+            fontFamily: 'Mulish',
+            fontSize: 21,
+            fontWeight: 600,
+            lineHeight: '26.36px'
+        },
+        h3: {
+            fontFamily: 'Mulish',
+            fontSize: 16,
+            fontWeight: 600,
+            lineHeight: '20px'
+        },
+        subtitle1: {
+            fontFamily: 'Mulish',
+            fontSize: 18,
+            fontWeight: 600,
+            lineHeight: '20px'
+        },
+        body1: {
+            fontFamily: 'Mulish',
+            fontSize: 16,
+            fontWeight: 400,
+            lineHeight: '20px'
+        },
+        body2: {
+            fontFamily: 'Mulish',
+            fontSize: 14,
+            fontWeight: 300,
+            lineHeight: '18px'
+        },
+        button: {
+            fontFamily: 'Mulish',
+            fontSize: 16,
+            fontWeight: 'bold',
+            lineHeight: '20px'
+        },
+        button2: {
+            fontFamily: 'Mulish',
+            fontSize: 12,
+            fontWeight: 'bold',
+            lineHeight: '15px'
+        },
+        caption: {
+            fontFamily: 'Mulish',
+            fontSize: 12,
+            fontWeight: 300,
+            lineHeight: '15px'
+        }
+    },
+    palette: {
+        secondary: {
+            main: COLORS.secondary
+        },
+        text: {
+            primary: COLORS.textMain,
+        },
+        warning: {
+            main: COLORS.warning
+        },
+        error: {
+            main: COLORS.error
+        },
+    },
+})
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "[name].js",
+        chunkFilename: "[name].bundle.js"
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+        alias: {
+            Components: path.resolve(__dirname, './src/components'),
+            Constants: path.resolve(__dirname, './src/constants'),
+            Pages: path.resolve(__dirname, './src/pages'),
+            Styles: path.resolve(__dirname, './src/style')
+        }
+    },
+    devServer: {
+        port: '3000',
+        hot: true,
+        watchFiles: ["./src/**"]
+    },
+
+    optimization: {
+        minimize: true,
+        runtimeChunk: true,
+        splitChunks: {
+            chunks: 'all',
+            cacheGroups: {
+                default: false,
+                reactVendor: {
+                    test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+                    chunks: 'all',
+                    name: "react-vendor",
+                    filename: "assets/common.[chunkhash].js",
+                    reuseExistingChunk: true,
+                    enforce: true,
+                    priority: 20,
+                },
+                vendors: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    filename: 'assets/vendor.[chunkhash].js',
+                    reuseExistingChunk: true,
+                    enforce: true,
+                    priority: 10,
+                }
+            }
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+                type: "asset",
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./public/index.html",
+            filename: "index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename: "assets/css/[name].css",
+        }),
+        new CleanWebpackPlugin(),
+        new BundleAnalyzerPlugin({
+            // generateStatsFile: true,
+            analyzerMode: 'disabled',
+            generateStatsFile: true
+        })
+    ]
+}

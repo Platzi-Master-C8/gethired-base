6878a0c85d5e118d81aa4da1acc71f4171134d63
Yadurani Lopez <yadu-lopez@hotmail.com>
2021-12-06 11:05:53 -0500

    feat: update version package theme, add package commons and icons

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 import Btn from '@mui/material/Button';
 
-const Button = () => <Btn>Button</Btn>;
+const Button = () => <Btn variant="contained">Button</Btn>;
 
 export default Button;
